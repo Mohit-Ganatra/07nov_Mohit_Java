f4bf090c4d61719f96fed74caf58d6d6e945cfa0
Mohit-Ganatra <118653620+Mohit-Ganatra@users.noreply.github.com>
2022-12-13 14:54:37 +0530

    Update YearToDays.c

--- Assignment/Module-3/Module--3.1/YearToDays.c
+++ Assignment/Module-3/Module--3.1/YearToDays.c
@@ -4,14 +4,33 @@
 
 int main()
 {
-	int year,days;
+	float year;
+	int days;
+	char a;
 	
-	printf("Enter no. of years = ");
-	scanf("%d",&year);
+	printf("\n d = year to days \n y = days to year \n " );
 	
-	days=year*365;
+	printf("\n Enter d or y = ");
+	scanf("%c",&a);
 	
-	printf(" %d Year = %d days",year,days);
-	
+	if(a=='d')
+	{
+		
+	    printf("\n Enter no. of years = ");
+	    scanf("%f",&year);
+	    days=year*365;
+	    printf(" %f Year = %d days",year,days);
+	}
+	else if(a=='y')
+	{
+	    printf("\n Enter no. of days = ");
+	    scanf("%d",&days);
+	    year=days/365;
+	    printf(" \n %d days = %f Year ",days,year);
+	}
+	else
+	{
+		printf(" \n Please enter a valid input......");
+	}
 	return 0;
 }
